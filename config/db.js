76d659e26f79f92efd88396adefcee4a4f8ef368
Diego <diegotozetto@gmail.com>
2020-06-10 20:54:21 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb://admin:admin1A@ds141168.mlab.com:41168/api-pizzabor"}
+    module.exports = {mongoURI: "mongodb://admin:admin1A@ds363098.mlab.com:63098/api-store"}
 }
-else{
+else{    
     module.exports = {mongoURI: "mongodb://localhost/db-store"}
 }
