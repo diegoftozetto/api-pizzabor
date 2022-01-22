34d6189dcf0d359d76a69720cc96de79e3945113
Diego F. Tozetto <diegotozetto@gmail.com>
2022-01-22 20:14:51 -0300

    Adicionando primeiro teste com Jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@src(.*)$': '<rootDir>/src$1',
+    '^@service(.*)$': '<rootDir>/src/service$1',
+    '^@controller(.*)$': '<rootDir>/src/controllers$1',
+  },
+};
---
+++ tests/controllers/client.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require(`@src/app`);
+const mongoose = require("mongoose");
+
+beforeEach((done) => {
+  mongoose.connect("mongodb://localhost/db-store",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => done());
+});
+
+afterEach((done) => {
+  mongoose.connection.db.dropDatabase(() => {
+    mongoose.connection.close(() => done())
+  });
+});
+
+describe('Client', () => {
+  test('should return all clients when the connection with the database occurs correctly', async () => {
+    const response = await request(app).get("/api/clients");
+    expect(response.status).toBe(200);
+  });
+})
