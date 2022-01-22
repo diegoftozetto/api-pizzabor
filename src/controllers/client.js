5c14f7e2bf6eefddb11b03e8a4bc1e8ed5335a3b
Diego F. Tozetto <diegotozetto@gmail.com>
2022-01-22 20:05:50 -0300

    modificando client.js

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -3,58 +3,56 @@ const mongoose = require("mongoose");
 const Client = mongoose.model("clients");
 
 class ClientController {
-  post(req, res) {
-    if (Object.keys(req.body).length === 0) {
-      res.status(400).json({ "message": "Falha ao processar requisição. Parâmetros Inválidos." });
-    } else {
-      var flag = true;
-      for (key in req.body) {
-        if (key != "complement") {
-          if (key == "number" && !Number.isInteger(req.body[key])) {
-            flag = false;
-          }
+  async post(req, res) {
+    if (!_validateInput()) {
+      res.status(400).send("Falha ao processar requisição. Parâmetros Inválidos.");
+    }
 
-          if (!req.body[key] || typeof req.body[key] == undefined || req.body[key] == null) {
-            flag = false;
-          }
-        }
-      }
-
-      if (!flag) {
-        res.status(400).json({ "message": "Falha ao processar requisição. Parâmetros Inválidos." });
-      }
-      else {
-        new Client({
-          name: req.body.name,
-          email: req.body.email,
-          phone: req.body.phone,
-          cep: req.body.cep,
-          address: req.body.address,
-          number: req.body.number,
-          complement: req.body.complement
-        }).save().then(() => {
-          res.status(201).json({ "message": "Cliente adicionado com sucesso." });
-        }).catch(() => {
-          res.status(500).json({ "message": "Falha ao processar requisição. Erro ao adicionar cliente no Database." });
-        });
-      }
+    const client = new Client(req.body);
+    try {
+      await client.save();
+      res.status(201).send(client);
+    } catch (error) {
+      res.status(500).send(err.message);
     }
   }
 
-  get(req, res) {
-    Client.find().sort({ name: 'asc' }).then((clients) => {
-      res.status(200).json(clients);
-    }).catch(() => {
-      res.status(500).json({ "message": "Falha ao processar requisição. Erro ao buscar cliente no Database." });
-    });
+  _validateInput() {
+    if (Object.keys(req.body).length === 0) {
+      return false;
+    }
+
+    var flag = true;
+    for (key in req.body) {
+      if (key != "complement") {
+        if (key == "number" && !Number.isInteger(req.body[key])) {
+          return false;
+        }
+        if (!req.body[key] || typeof req.body[key] == undefined || req.body[key] == null) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
-  delete(req, res) {
-    Client.deleteOne({ _id: req.params.id }).then(() => {
-      res.status(200).json({ "message": "Cliente removido com sucesso." });
-    }).catch(() => {
-      res.status(500).json({ "message": "Falha ao processar requisição. Erro ao remover cliente no Database." });
-    });
+  async get(req, res) {
+    try {
+      const clients = await Client.find().sort({ name: 'asc' });
+      res.status(200).send(clients);
+    } catch (error) {
+      res.status(500).send(err.message);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      await Client.deleteOne({ _id: req.params.id });
+      res.sendStatus(204);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
 }
 
