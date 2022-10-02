b7ea0dbd77fbda88ba937987386e87e934e64ce5
Diego Tozetto <diegotozetto@gmail.com>
2022-10-02 00:36:59 -0300

    test: ajusta teste para validação de cep

--- tests/controllers/ClientController.test.js
+++ tests/controllers/ClientController.test.js
@@ -17,72 +17,70 @@ afterEach((done) => {
   });
 });
 
-describe('ClientController - Post', () => {
-  const body = {
-    'name': 'Teste Name',
-    'email': 'teste@email.com',
-    'phone': '(41)99090-99089',
-    'cep': '8023100',
-    'address': 'Address',
-    'number': '400',
-    'complement': 'Complement'
-  };
-
-  test('should add client when the connection with the database occurs correctly', async () => {
-    const reponseData = {
-      '_id': '532a43d204945946489f8285',
-      ...body,
-      '__v': 0
+describe('ClientController', () => {
+  describe('ClientController - Post', () => {
+    const body = {
+      'name': 'Teste Name',
+      'email': 'teste@email.com',
+      'phone': '(41)99090-99089',
+      'cep': '80230-000'
     };
 
-    const spyCreateClient = jest.spyOn(ClientService, 'createClient').mockImplementation(() => reponseData);
-
-    const response = await request(app).post('/api/clients').send(body);
-
-    expect(spyCreateClient).toHaveBeenCalledTimes(1);
-    expect(response.status).toBe(StatusCodes.CREATED);
-    expect(response.body).toEqual(reponseData);
-    spyCreateClient.mockRestore();
-  });
-
-  test('Should return internal server error when unexpected error occurs while saving client', async () => {
-    const error = {
-      message: 'There was an error saving client.'
-    };
-
-    const spyCreateClient = jest.spyOn(ClientService, 'createClient').mockRejectedValue(error);
-
-    const response = await request(app).post('/api/clients').send(body);
-
-    expect(spyCreateClient).toHaveBeenCalledTimes(1);
-    expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
-    expect(response.text).toEqual(error.message);
-    spyCreateClient.mockRestore();
-  })
-});
-
-describe('ClientController - Get', () => {
-  test('should return all clients when the connection with the database occurs correctly', async () => {
-    const reponseDataExpected = [
-      {
-        '_id': '633320a79f59e30a7cb58b57',
-        'url': 'URL',
-        'name': 'Test Name',
-        'categorie': 'Teste Categorie',
-        'price': '10',
-        'description': 'Test Description',
+    test('should add client when the connection with the database occurs correctly', async () => {
+      const reponseData = {
+        '_id': '532a43d204945946489f8285',
+        ...body,
         '__v': 0
-      }
-    ];
+      };
 
-    const spyListAllClients = jest.spyOn(ClientService, 'listAllClients').mockImplementation(() => reponseDataExpected);
+      const spyCreateClient = jest.spyOn(ClientService, 'createClient').mockImplementation(() => reponseData);
 
-    const response = await request(app).get('/api/clients');
+      const response = await request(app).post('/api/clients').send(body);
 
-    expect(spyListAllClients).toHaveBeenCalledTimes(1);
-    expect(response.status).toBe(StatusCodes.OK);
-    expect(response.body).toEqual(reponseDataExpected);
-    spyListAllClients.mockRestore();
+      expect(spyCreateClient).toHaveBeenCalledTimes(1);
+      expect(response.status).toBe(StatusCodes.CREATED);
+      expect(response.body).toEqual(reponseData);
+      spyCreateClient.mockRestore();
+    });
+
+    test('Should return internal server error when unexpected error occurs while saving client', async () => {
+      const error = {
+        message: 'There was an error saving client.'
+      };
+
+      const spyCreateClient = jest.spyOn(ClientService, 'createClient').mockRejectedValue(error);
+
+      const response = await request(app).post('/api/clients').send(body);
+
+      expect(spyCreateClient).toHaveBeenCalledTimes(1);
+      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+      expect(response.text).toEqual(error.message);
+      spyCreateClient.mockRestore();
+    })
+  });
+
+  describe('ClientController - Get', () => {
+    test('should return all clients when the connection with the database occurs correctly', async () => {
+      const reponseDataExpected = [
+        {
+          '_id': '633320a79f59e30a7cb58b57',
+          'url': 'URL',
+          'name': 'Test Name',
+          'categorie': 'Teste Categorie',
+          'price': '10',
+          'description': 'Test Description',
+          '__v': 0
+        }
+      ];
+
+      const spyListAllClients = jest.spyOn(ClientService, 'listAllClients').mockImplementation(() => reponseDataExpected);
+
+      const response = await request(app).get('/api/clients');
+
+      expect(spyListAllClients).toHaveBeenCalledTimes(1);
+      expect(response.status).toBe(StatusCodes.OK);
+      expect(response.body).toEqual(reponseDataExpected);
+      spyListAllClients.mockRestore();
+    });
   });
 });
-
